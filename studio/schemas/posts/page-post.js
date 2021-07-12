017c277b40e33ab2dab516c8b5e657415569fad3
Joaqim Planstedt <github@joaqim.xyz>
2021-07-12 10:16:21 +0200

    Add Posts/Blog schema

---
+++ studio/schemas/posts/page-post.js
@@ -0,0 +1,53 @@
+export default {
+  title: 'Blog Page',
+  name: 'blogPage',
+  type: 'document',
+  __experimental_actions: ['update', 'publish'], // disable for initial publish
+  fields: [
+    {
+      title: 'Overlay header with transparency?',
+      name: 'hasTransparentHeader',
+      type: 'boolean',
+      description:
+        'When toggled on, the header will appear with a transparent background over the first content module and text/logos will be white until scrolling is engaged.'
+    },
+    {
+      title: 'Page Modules',
+      name: 'modules',
+      type: 'array',
+      of: [
+        { type: 'grid' },
+        { type: 'hero' },
+        { type: 'marquee' },
+        { type: 'dividerPhoto' }
+      ]
+    },
+    {
+      title: 'Featured Posts',
+      name: 'featuredPosts',
+      description:
+        'Show these posts first, before sorting remaining products alphabetically ', //TODO: Sort by date
+      type: 'array',
+      of: [
+        {
+          title: 'Posts',
+          type: 'reference',
+          to: [{ type: 'post' }]
+        }
+      ],
+      validation: Rule => Rule.unique()
+    },
+    {
+      title: 'SEO / Share Settings',
+      name: 'seo',
+      type: 'seo'
+    }
+  ],
+  preview: {
+    prepare() {
+      return {
+        title: 'Blog Page'
+      }
+    }
+  }
+}
