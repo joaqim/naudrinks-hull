9f158da429a2503bc88a5470bbe4b1e0bc271a5e
Joaqim Planstedt <github@joaqim.xyz>
2021-08-26 10:21:18 +0200

    Add post-grid.stories.js

--- src/blocks/blog/post-grid.js
+++ src/blocks/blog/post-grid.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PostCard from './post-card'
 
 const PostGrid = ({ posts }) => {
+  if (!posts) return <div></div>
   return (
     <section className="section py-4">
       <div className="flex flex-wrap mx-0 sm:mx-20 transition-all duration-150">
---
+++ src/blocks/blog/post-grid.stories.tsx
@@ -0,0 +1,46 @@
+import { Meta, Story } from '@storybook/react'
+import PostGrid from './post-grid'
+import DefaultCard from './post-card'
+
+export default {
+  component: PostGrid,
+  title: 'Post Grid',
+} as Meta
+
+export const DefaultGrid = (args) => <PostGrid {...args}></PostGrid>
+
+const postMock = () => {
+  return {
+    slug: { _type: 'slug', current: 'test-post' },
+    description: { _type: 'description', en: 'Description' },
+    createdAt: '2021-08-19 10:49',
+    title: { _type: 'title', en: 'Title' },
+    author: {
+      name: 'Author Name',
+      slug: { current: 'author-name' },
+      image: {
+        asset: {
+          _type: 'reference',
+          _ref: 'image-d262062282136f514ec48f6d2f79409827d3ed43-670x870-png',
+        },
+      },
+    },
+    categories: [
+      {
+        title: { en: 'Placeholder Category' },
+        slug: { current: 'placeholder-category' },
+      },
+    ],
+    mainImage: {
+      _type: 'image',
+      asset: {
+        _type: 'reference',
+        _ref: 'image-ed2c93f17f9243d190f9ececeb8691701e42d04e-3840x2560-jpg',
+      },
+    },
+  }
+}
+
+DefaultGrid.args = {
+  posts: Array(5).fill(postMock()),
+}
